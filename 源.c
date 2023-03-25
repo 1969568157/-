56285b87c2e7468f10558e4812966c170797e241
1969568157 <128714372+1969568157@users.noreply.github.com>
2023-03-25 13:22:44 +0800

    Add files via upload

---
+++ 源.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a', 'b', 'c' };
+//	int str = strlen("ABC");
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+}
